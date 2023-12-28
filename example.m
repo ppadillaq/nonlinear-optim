bffbf9c701c005becbaf5eb545bd203c198e25f8
ppadillaq <pedro.padilla@sunamp.com>
2023-12-28 11:10:24 +0100

    Removes function definition from example. Now it is loaded from a file. Improves plots.

--- example.m
+++ example.m
@@ -7,11 +7,8 @@
 clear
 clc
 % Unconstrained minimization problem
-% Cost function
-f=@(x) x(:,1) - x(:,2) + 2*x(:,1).^2 + 2*x(:,1).*x(:,2) +x(:,2).^2;
-% Gradient
-grad ={@(x) 1 + 4*x(1) + 2*x(2) , ...
-       @(x) -1 + 2*x(1) + 2*x(2)};
+% Cost function (and gradient)
+f = @quadratic;
 
 %% Plot mesh
 x = linspace(-2,1,100);
@@ -19,15 +16,17 @@ y = linspace(-1,3,100);
 [m_x, m_y] = meshgrid(x,y);
 z = f([m_x(:) m_y(:)]);
 mesh(x,y,reshape(z,[numel(x) numel(y)]))
-%figure, contour(x,y,reshape(z,[numel(x) numel(y)]), 30)
+figure, contour(x,y,reshape(z,[numel(x) numel(y)]), 50)
 hold on
 
 %% Optimization
 % Starting point
 X0 = [0 0];
-[X_sD,S_sD,fX_sD,i_sD] = steepestDescent(f,grad, X0);
+[X_sD,S_sD,fX_sD,i_sD] = steepestDescent(f, X0);
 scatter3(X_sD(:,1),X_sD(:,2),f(X_sD),'filled', 'MarkerFaceColor', 'red')
+quiver(X_sD(:,1),X_sD(:,2),S_sD(:,1),S_sD(:,2),'r')
 
-[X_cG,S_cG,fX_cG,i_cG] = conjugateGradient(f,grad, X0);
+[X_cG,S_cG,fX_cG,i_cG] = conjugateGradient(f, X0);
 scatter3(X_cG(:,1),X_cG(:,2),f(X_cG),'filled', 'MarkerFaceColor', 'green')
+quiver(X_cG(:,1),X_cG(:,2),S_cG(:,1),S_cG(:,2),'g')
 
