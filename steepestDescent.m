7c06023d2f1a9f96c302c5497edf9ff1b6cc271c
ppadillaq <pedro.padilla@sunamp.com>
2023-12-28 11:00:24 +0100

    Adds 'varargin' to modify default parameters (maximum of iterations and default 1-dim minimization method). Removes 'gradient' from the arguments.

--- steepestDescent.m
+++ steepestDescent.m
@@ -1,27 +1,54 @@
-function [X,S,fX,i] = steepestDescent(f,gradient, X0)
+function [X,S,fX,i] = steepestDescent(f, X0, varargin)
     %STEEPESTDESCENT Summary of this function goes here
     %   Detailed explanation goes here
     %   Pedro Padilla Quesada
     
-    maxIter = 100; % maximum no. of iterations
+    maxIter = 1e4; % maximum no. of iterations
     converged = [0 0 0]; % convergence flags (3 criteria)
     i = 1; % iteration counter
+
+    % One-dimensional minimization default method: Dichotomous Search
+    delta = 1e-6; % eval margin
+    tol = 1e-7; % error tolerance
+    oneDimSearch = @(f) dichoSearch(f,0, 10, delta, tol);
+
+    % is optional arguments are introduced:
+    if ~isempty(varargin)
+        props = varargin(1:2:numel(varargin));
+        values = varargin(2:2:numel(varargin));
+        for j = 1:numel(props)
+            switch props{j}
+                case 'maxIter'
+                    maxIter = values{j};
+                case 'oneDimSearch'
+                    if values{j} == "dichoSearch"
+                        % default method
+                    elseif values{j} == "quasiNewton1dim"
+                        step = 0.01;
+                        oneDimSearch = @(f) quasiNewton1dim(f, 0, step);
+                    end
+                otherwise
+                    disp('Unknown function argument.')
+            end
+        end
+    end
 
     % Iterations matrix - Preallocation
     X = NaN(maxIter, numel(X0));
     S = NaN(maxIter, numel(X0));
     fX = NaN(maxIter, 1);
     X(i,:) = X0;
-    fX(i) = f(X0);
+    [fX(i), delta_f] = f(X0);
     
     while ~any(converged) && i < maxIter
         fprintf('Iteration %d...\n',i)
-        delta_f = [gradient{1}(X(i,:)) gradient{2}(X(i,:))];
+        %delta_f = [gradient{1}(X(i,:)) gradient{2}(X(i,:))];
         S(i,:) = -1*delta_f;
         wrapper = @(lambda) wrapper1dim(f, lambda, X(i,:), S(i,:));
-        [lambda_opt, fX(i+1), ~, ~, ~, ~, ~] = dichoSearch(wrapper,0, 100, 0.000001, 0.0000001);
+        [lambda_opt, fX(i+1)] = oneDimSearch(wrapper);
         X(i+1,:) = X(i,:) + lambda_opt * S(i,:);
-        converged = convergenceCriteria(X(i+1,:), X(i,:), fX(i+1),fX(i),delta_f, 0.000001,0.001,0.001);
+        [~, delta_f] = f(X(i+1,:));
+        converged = convergenceCriteria(X(i+1,:), X(i,:), fX(i+1),fX(i),delta_f, 1e-8,1e-8,1e-8);
     
         i = i + 1;
     end
