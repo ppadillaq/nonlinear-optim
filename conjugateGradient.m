6e6c81d72b4563ef5ef51ee51e672586933a633c
ppadillaq <pedro.padilla@sunamp.com>
2023-12-28 09:25:30 +0100

    - Removes 'gradient' argument (gradient is included now at test functions). - Increases maxIter to 10000.

--- conjugateGradient.m
+++ conjugateGradient.m
@@ -1,9 +1,9 @@
-function [X,S,fX,i] = conjugateGradient(f,gradient, X0)
+function [X,S,fX,i] = conjugateGradient(f, X0)
     %CONJUGATEGRADIENT Summary of this function goes here
     %   Detailed explanation goes here
     %   Pedro Padilla Quesada
     
-    maxIter = 100; % maximum no. of iterations
+    maxIter = 1e4; % maximum no. of iterations
     converged = [0 0 0]; % convergence flags (3 criteria)
     i = 1; % iteration counter
 
@@ -13,24 +13,26 @@ function [X,S,fX,i] = conjugateGradient(f,gradient, X0)
     fX = NaN(maxIter, 1);
     delta_f = NaN(maxIter, numel(X0));
     X(i,:) = X0;
-    fX(i) = f(X0);
+    [fX(i), delta_f(i,:)] = f(X0);
 
     % First search direction
-    delta_f(i,:) = [gradient{1}(X(i,:)) gradient{2}(X(i,:))];
+    %delta_f(i,:) = [gradient{1}(X(i,:)) gradient{2}(X(i,:))];
     S(i,:) = -1*delta_f(i,:);
     wrapper = @(lambda) wrapper1dim(f, lambda, X(i,:), S(i,:));
     [lambda_opt, fX(i+1), ~, ~, ~, ~, ~] = dichoSearch(wrapper,0, 100, 0.000001, 0.0000001);
     X(i+1,:) = X(i,:) + lambda_opt * S(i,:);
+    [~, delta_f(i+1,:)] = f(X(i+1,:));
     i = i + 1;
     
     while ~any(converged) && i < maxIter
         fprintf('Iteration %d...\n',i)
-        delta_f(i,:) = [gradient{1}(X(i,:)) gradient{2}(X(i,:))];
+        %delta_f(i,:) = [gradient{1}(X(i,:)) gradient{2}(X(i,:))];
         S(i,:) = -1*delta_f(i,:) + norm(delta_f(i,:))^2/norm(delta_f(i-1,:))^2 * S(i-1,:);
         wrapper = @(lambda) wrapper1dim(f, lambda, X(i,:), S(i,:));
         [lambda_opt, fX(i+1), ~, ~, ~, ~, ~] = dichoSearch(wrapper,0, 100, 0.000001, 0.0000001);
         X(i+1,:) = X(i,:) + lambda_opt * S(i,:);
-        converged = convergenceCriteria(X(i+1,:), X(i,:), fX(i+1),fX(i),delta_f(i,:), 0.000001,0.001,0.001);
+        [~, delta_f(i+1,:)] = f(X(i+1,:));
+        converged = convergenceCriteria(X(i+1,:), X(i,:), fX(i+1),fX(i),delta_f(i,:), 1e-6,1e-6,1e-6);
         i = i + 1;
     end
     
